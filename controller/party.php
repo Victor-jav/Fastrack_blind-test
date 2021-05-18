ed728518a779a1b9a224bc74bbc3aedbb8b32841
Angel <angelcp38@gmail.com>
2021-05-18 12:47:24 +0200

    u

--- controller/party.php
+++ controller/party.php
@@ -2,4 +2,10 @@
 if(empty($_SESSION['id_user'])){
 
     $page_css = "\"./public/style_party.css\"";
+    $title = "Party";
+    ob_start();
+//ici
+$content = ob_get_clean();
+require('./view/afficher_lobby.php');
+
 }
