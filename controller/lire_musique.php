e66f2cdacf11bd6c218bbb8b08664b93e9ed6630
Angel <angelcp38@gmail.com>
2021-05-27 12:00:57 +0200

    o

--- controller/lire_musique.php
+++ controller/lire_musique.php
@@ -14,7 +14,12 @@ function db_connect()
         echo "bug lors de la co ac la bdd";
     }
 }
-
+<form name='fo' action="" method='post' enctype='multipart/form-data'>
+                <label><b>musique : </b></label><br>
+                <input type="file" name='son'><br><br>
+                <input type="submit" name='valider' value='charger'>
+            </form>
+            
 $son = fopen("")
 if (!$son) {
     echo "<p>Impossible de lire la page.\n";
