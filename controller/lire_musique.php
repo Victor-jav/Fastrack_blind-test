952579c1737c301a664d5c1f88a7c85ecf44b0e0
Angel <angelcp38@gmail.com>
2021-05-27 12:07:31 +0200

    o

--- controller/lire_musique.php
+++ controller/lire_musique.php
@@ -1,39 +1,5 @@
 <?php
 
-function db_connect()
-{
-    try
-    {
-        $pdo = new PDO('mysql:host=localhost;dbname=fastrack','root','');
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-        //echo 'connextion réussie';
-        return $pdo;
-    }
-    catch (PDOException $e)
-    {
-        echo "bug lors de la co ac la bdd";
-    }
-}
-<form name='fo' action="" method='post' enctype='multipart/form-data'>
-                <label><b>musique : </b></label><br>
-                <input type="file" name='son'><br><br>
-                <input type="submit" name='valider' value='charger'>
-            </form>
-            
-$son = fopen("")
-if (!$son) {
-    echo "<p>Impossible de lire la page.\n";
-    exit;
-  }
-  while (!feof ($son)) {
-      $line = fgets ($son, 1024);
-      /* Cela ne fonctionne que si les balises Title sont correctement utilisées */
-      if (preg_match ("@\<title\>(.*)\</title\>@i", $line, $out)) {
-          $title = $out[1];
-          break;
-      }
-  }
-  fclose($son);
 
 
   ?>
