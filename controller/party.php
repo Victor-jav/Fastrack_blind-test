5e70235bc71af673447f22337a8a2a5b85e7d760
Fumetsu-hub <angelcp38@gmail.com>
2021-05-31 10:41:28 +0200

    o

--- controller/party.php
+++ controller/party.php
@@ -4,7 +4,7 @@ if(empty($_SESSION['id_user'])){
     $page_css = "\"./public/style_party.css\"";
     $title = "Party";
     ob_start();
-//ici
+
 $content = ob_get_clean();
 require('./view/afficher_lobby.php');
 
