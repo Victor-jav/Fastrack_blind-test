5dedcee8c6f44d4d2a5951973f490dde820db8c5
MIMIR-X <clement.bovin@gmail.com>
2021-06-14 19:41:33 +0200

    condition user exist register

--- controller/register.php
+++ controller/register.php
@@ -1,13 +1,34 @@
 <?php
+$bdd = new PDO('mysql:host=localhost;dbname=fastrack','root','');
 
 if(isset($_POST['action']))
 {
     if(!empty($_POST['prenom']) AND !empty($_POST['nom']) AND !empty($_POST['identifiant']) AND !empty($_POST['mdp']) AND !empty($_POST['mail']))
     {
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $nom = htmlspecialchars($_POST['nom']);
+        $identifiant = htmlspecialchars($_POST['identifiant']);
+        $mdp = sha1($_POST['mdp']);
+        $mail = htmlspecialchars($_POST['mail']);
+
+        //condition si l'utilisateur existe deja il n'est pas ajouté à la bdd ( j'ai pas réussis à le faire ne function)
+        $user_check = $bdd -> prepare("SELECT id_user FROM user WHERE identifiant = ?");
+        $user_check->execute([$identifiant]);
+    
+        $exist = $user_check->rowCount();
+    
+        if($exist == 0) 
+        {
         require('model\register.php');
         $succes = "Votre compte a été créé avec succès !";
+
         //pour rediriger automatiquement
         //header('Refresh: 3; URL=index.php');
+        }
+        else 
+        {
+            $erreur2 = "Le nom d'utilisateur existe déjà !";
+        }
     }
     else 
     {
--- model/register.php
+++ model/register.php
@@ -4,17 +4,11 @@
 $connection= mysqli_connect("localhost", "root", "", "fastrack");
 
 if (!$connection){ // Contrôler la connexion
-    $MessageConnexion = die ("connection impossible");
+    $MessageConnexion = die ("Connection impossible");
 }
 
 else {
 if(isset($_POST['prenom']) && $_POST['nom'] && $_POST['identifiant'] && $_POST['mdp'] && $_POST['mail']) {
-
-        $prenom = htmlspecialchars($_POST['prenom']);
-        $nom = htmlspecialchars($_POST['nom']);
-        $identifiant = htmlspecialchars($_POST['identifiant']);
-        $mdp = htmlspecialchars(sha1($_POST['mdp']));
-        $mail = htmlspecialchars($_POST['mail']);
 
         $Ajouter="INSERT INTO user (id_user, prenom, nom, identifiant, mdp, mail) VALUES (NULL, '$prenom', '$nom', '$identifiant', '$mdp', '$mail')";
    
