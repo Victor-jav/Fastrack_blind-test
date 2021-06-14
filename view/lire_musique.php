805d1e7682e2ecaf1769d3f51d07720f975f7d21
Fumetsu-hub <angelcp38@gmail.com>
2021-06-14 12:37:42 +0200

    o

--- view/lire_musique.php
+++ view/lire_musique.php
@@ -7,3 +7,17 @@
 
 <?php
 
+    function db_connect()
+    {
+        try
+        {
+            $pdo = new PDO('mysql:host=localhost;dbname=fastrack','root','');
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+            //echo 'connextion réussie';
+            return $pdo;
+        }
+        catch (PDOException $e)
+        {
+            echo "bug lors de la co ac la bdd";
+        }
+    }
